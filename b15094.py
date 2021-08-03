cd9456730d3273d249b82cce9c01aa1c6a3f422d
eora21 <eora21@naver.com>
2021-08-03 22:23:52 +0900

    clean

--- b15094.py
+++ b15094.py
@@ -2,14 +2,14 @@ st = input()
 UCPC = "UCPC"
 
 i = 0
-j = 0
+cnt = 0
 
-while(j < 4 and i < len(st)):
-    if st[i] == UCPC[j]:
-        j += 1
+while(cnt < 4 and i < len(st)):
+    if st[i] == UCPC[cnt]:
+        cnt += 1
     i += 1
 
-if j >= 4:
+if cnt >= 4:
     print("I love UCPC")
 else :
     print("I hate UCPC")
