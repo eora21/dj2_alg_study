54379c76e8222766f4d761ec34be485cbb68cb2f
eora21 <eora21@naver.com>
2021-08-07 11:56:33 +0900

    10162

---
+++ b10162.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+button = [300, 60, 10]
+check = [0, 0, 0]
+for i in range(3):
+    check[i] = T // button[i]
+    T %= button[i]
+
+if T == 0:
+    print(*check)
+else:
+    print(-1)
