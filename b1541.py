8edb9bb048aa3e6bea94792f577e2d713948f9ea
eora21 <eora21@naver.com>
2021-08-08 18:39:34 +0900

    1541

--- b1541.py
+++ b1541.py
@@ -1,32 +1,3 @@
-def find_max_total(f_li, mode):
-    if mode:
-        for s in range(len(f_li)):
-            if f_li[s] == "+":
-                f_li[s] = "-"
-            elif f_li[s] == "-":
-                f_li[s] = "+"
-    total = 0
-    plus = True
-    flag = True
-    total_min = 0
-    if len(f_li) == 1:
-        return f_li[0]
-    for j in range(len(f_li)):
-        if type(f_li[j]) is int:
-            if plus:
-                total += f_li[j]
-            else:
-                total -= f_li[j]
-        elif f_li[j] == "-":
-            plus = False
-            if flag:
-                total_min = total - find_max_total(f_li[j+1:], True)
-                flag = False
-        else:
-            plus = True
-    return total if total < total_min else total_min
-
-
 string = input()
 li = []
 cut = 0
@@ -37,4 +8,16 @@ for i in range(len(string)):
         cut = i + 1
 li.append(int(string[cut:]))
 
-print(find_max_total(li, False))
+total = 0
+mode = True
+for j in range(len(li)):
+    if type(li[j]) is int:
+        if mode:
+            total += li[j]
+        else:
+            total -= li[j]
+            
+    elif mode and li[j] == "-":
+        mode = False
+
+print(total)
