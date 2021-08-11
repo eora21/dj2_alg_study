663d0c408cdd82a499b726a04a9afca941801122
eora21 <eora21@naver.com>
2021-08-11 15:20:56 +0900

    13305

--- b13305.py
+++ b13305.py
@@ -1,26 +1,13 @@
 C = int(input())
 Km = list(map(int, input().split()))
 L = list(map(int, input().split()))
-_L = L[:]
+
+oil = L[0]
 total = 0
 
-min_L = min(L[:-1])
-
-min_L_idx = []
-
-i = 0
-while True:
-    if min_L == _L[i]:
-        min_L_idx.insert(0, i)
-        _L = _L[:i]
-        if not len(_L):
-            break
-        min_L = min(_L)
-        i = -1
-    i += 1
-# print(min_L)
-for k in range(len(min_L_idx) - 1):
-    total += sum(Km[min_L_idx[k]:min_L_idx[k + 1]]) * L[min_L_idx[k]]
-total += sum(Km[min_L_idx[-1]:]) * L[min_L_idx[-1]]
+for i in range(1, len(L)):
+    total += oil * Km[i - 1]
+    if oil > L[i]:
+        oil = L[i]
 
 print(total)
