45e77b10aba68142705d163f4ad301d76ab30fb1
eora21 <eora21@naver.com>
2021-08-15 20:50:33 +0900

    1931

--- b1931.py
+++ b1931.py
@@ -1,21 +1,19 @@
-T = int(input())
+from sys import stdin
 
-dic = {}
+T = int(stdin.readline())
 
-for case in range(T):
-    StartTime, EndTime = map(int, input().split())
-    try:
-        dic[EndTime - StartTime].append(StartTime)
-    except:
-        dic[EndTime - StartTime] = [StartTime]
+li = []
+for _ in range(T):
+    li.append(list(map(int, stdin.readline().split())))
 
-sort_li = sorted(dic.items(), key= lambda item: item[0])
+li.sort(key=lambda x : (x[1], x[0]))
 
-print(sort_li)
+StartTime = 0
+cnt = 0
 
-"""
-나중에 풀어볼 것, 개어려움
-1~8, 7~9, 8~112
-7~9를 선택하는 것보다 나머지 2개를 선택하는게 더 좋음
-야.. 이걸 어케 생각한담?
-"""
+for i in range(len(li)):
+    if StartTime <= li[i][0]:
+        cnt += 1
+        StartTime = li[i][1]
+
+print(cnt)
