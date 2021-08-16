3e00f4a9699bc4f147cff109e4e8a23b925a9375
eora21 <eora21@naver.com>
2021-08-16 15:39:42 +0900

    11497 faster, shorter

--- b11497.py
+++ b11497.py
@@ -4,15 +4,11 @@ for _ in range(T):
     N = int(input())
     wood = list(map(int, input().split()))
 
-    wood.sort()
-    li = [0] * N
+    wood.sort(reverse=True)
 
-    for i in range(N):
-        li[i // 2 + (i % 2) * (N - 1 +(- 2) * (i // 2))] = wood[i] #홀수일때 N - 1 - i // 2
-    
-    cnt = abs(li[0] - li[-1])
-    for w in range(N - 1):
-        if cnt < abs(li[w] - li[w + 1]):
-            cnt = abs(li[w] - li[w + 1])
+    cnt = abs(wood[0] - wood[1])
+    for w in range(N - 2):
+        if cnt < abs(wood[w] - wood[w + 2]):
+            cnt = abs(wood[w] - wood[w + 2])
 
     print(cnt)
