feb79cbfb114df375246320c83960d6a73769bdd
eora21 <eora21@naver.com>
2021-08-25 23:53:05 +0900

    12034_shorter

--- b12034.py
+++ b12034.py
@@ -3,14 +3,15 @@ for case in range(int(input())):
     li = list(map(int, input().split()))
     result = []
     
-    i = 0
+    i = 1
     while li:
-        if not li[i] % 3 and li[i] // 3 * 4 in li:
-            result.append(li[i])
-            li.remove(li[i] // 3 * 4)
-            del(li[i])
+        if li[0] // 3 * 4 == li[i]:
+            result.append(li[0])
+            del[li[i]]
+            del(li[0])
+            i = 1
         else:
-            i = (i + 1) % len(li)
+            i += 1
 
     print("Case #{}: ".format(case + 1), end="")
     print(*result)
