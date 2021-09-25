65769cfab76f6547f8f588a7c1cd456312578ec7
eora21 <eora21@naver.com>
2021-09-25 20:01:40 +0900

    14889

--- b14889.py
+++ b14889.py
@@ -1,33 +1,42 @@
-def make_team(num=0, human=0):
-    global min_minus_status
-
-    # if min_minus_status == 0:
-    #     return 0
-
+def make_start(pick=0, num=0):
     if num == N // 2:
-        print(team)
+        make_link(pick, num)
+        # print(start)
+        return
+    
+    for i in range(num, N//2):
+        team[i] = 0
+        make_start(pick+1, i+1)
+        team[i] = 1
 
-        status = [0] * 2
-        for check in range(2):
-            for i in range(N - 1):
-                for j in range(i+1, N):
-                    if team[i] == check and team[j] == check:
-                        status[check] += li[i][j] + li[j][i]
-        
-        minus_status = abs(status[0] - status[1])
 
-        if min_minus_status > minus_status:
-            min_minus_status = minus_status
-
+def make_link(pick=0, num=0):
+    if pick == N // 2:
+        make_score()
         return
 
-    for i in range(human, N):
-        if team[i]:
+    if num == N:
+        return
+    
+    for i in range(num, N):
             team[i] = 0
-            make_team(num+1, i+1)
+            make_link(pick+1, i+1)
             team[i] = 1
-            
-    return min_minus_status
+
+    make_link(pick, i + 1)
+
+
+def make_score():
+    status = [0] * 2
+    for i in range(N):
+        for j in range(N):
+            if team[i] == team[j]:
+                status[team[i]] += li[i][j]
+
+    global min_minus_status
+    minus_status = abs(status[0] - status[1])
+    if min_minus_status > minus_status:
+        min_minus_status = minus_status
 
 
 N = int(input())
@@ -37,6 +46,6 @@ li = [list(map(int, input().split())) for _ in range(N)]
 team = [1] * N
 min_minus_status = 100 * N
 
-print(make_team())
+make_start()
 
-#시간초과
+print(min_minus_status)
