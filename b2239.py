7b3c6fcb2a6b92f5698136bef40aed0aecf84c44
eora21 <eora21@naver.com>
2021-09-29 00:34:17 +0900

    b2239

--- b2239.py
+++ b2239.py
@@ -1,70 +1,35 @@
-def delete_sudoku(d):
-    for i in range(9):
-        for j in range(9):
-            if sudoku[i][j] >= 10 * d:
-                sudoku[i][j] = 0
-    return False
+def make_sudoku():
+    nums = [True] * 10
+    nums[0] = False
+    
+    for row in range(0, 9):
+        for col in range(0, 9):
+            if sudoku[row][col] == 0:
+                horizontal = sudoku[row]
+                for i in horizontal:
+                    nums[i] = False
 
+                vertical = [sudoku[y][col] for y in range(9)]
+                for j in vertical:
+                    nums[j] = False
 
-def make_sudoku(d=0):
-    make_min = False
+                r = row // 3 * 3
+                c= col // 3 * 3
+                square = [sudoku[y][x] for y in range(r, r+3) for x in range(c, c+3)]
+                for k in square:
+                    nums[k] = False
 
-    while True:
-        flag = True
-        not_changed = True
-
-        for row in range(9):
-            for col in range(9):
+                for check in range(1, 10):
+                    if nums[check]:
+                        sudoku[row][col] = check
+                        if make_sudoku():
+                            return True
+                        sudoku[row][col] = 0
+                
                 if sudoku[row][col] == 0:
-                    flag = False
-                    horizontal = set(sudoku[row])
-                    vertical = set(sudoku[y][col] for y in range(9))
-                    r = row // 3 * 3
-                    c= col // 3 * 3
-                    square = set(sudoku[y][x] for y in range(r, r+3) for x in range(c, c+3))
-                    
-                    not_possible_set = horizontal | vertical | square
+                    return False
 
-                    while True:
-                        repeat = False
-                        for i in not_possible_set:
-                            if i > 10:
-                                not_possible_set.remove(i)
-                                not_possible_set.add(i % 10)
-                                repeat = True
-                                break
-
-                        if not repeat:
-                            break
-                    
-                    possible_set = OtoN - not_possible_set
-
-                    if not possible_set:
-                        delete_sudoku(d)
-                        return False
-
-                    elif len(possible_set) == 1:
-                        sudoku[row][col] = 10 * d + (possible_set.pop())
-                        not_changed = False
-
-                    elif make_min:
-                        li = sorted(list(possible_set))
-                        for num in li:
-                            sudoku[row][col] = 10 * (d+1) + num
-                            if make_sudoku(d+1):
-                                return True
-                        else:
-                            delete_sudoku(d)
-                            return False
-
-        if flag:
-            return True
-        
-        elif not_changed:
-            make_min = True
-
-
-OtoN = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    return True
 
 sudoku = [list(map(int, input())) for _ in range(9)]
 
@@ -72,5 +37,5 @@ make_sudoku()
 
 for i in range(9):
     for j in range(9):
-        print(sudoku[i][j] % 10, end="")
+        print(sudoku[i][j], end="")
     print()
