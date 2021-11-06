d2881ccc3fad4a542c4503c5bd38dea34f9ee4c5
eora21 <eora21@naver.com>
2021-11-06 19:07:44 +0900

    1106

--- python/b17298.py
+++ python/b17298.py
@@ -2,11 +2,15 @@ N = int(input())
 nums = list(map(int, input().split()))
 ans = [-1] * N
 
-for now in range(N - 1, -1, -1):
-    for target in range(now - 1, -1, -1):
-        if nums[now] > nums[target]:
-            ans[target] = nums[now]
-        else:
+stack = []
+for i in range(N - 1, -1, -1):
+    now = nums[i]
+    while stack:
+        if stack[-1] > now:
+            ans[i] = stack[-1]
             break
+        else:
+            del(stack[-1])
+    stack.append(now)
 
 print(*ans)
